c26605e6733e0f121c1f1c10e2fad696fd03c9f3
Humza Ghayas <humza.ghayas@flui.co.uk>
2025-09-19 15:42:19 +0500

    changes

--- src/models/productModel.js
+++ src/models/productModel.js
@@ -2,6 +2,11 @@ import mongoose from "mongoose";
 
 const productSchema = new mongoose.Schema(
   {
+    _id: {
+      type: mongoose.Schema.Types.ObjectId,
+      auto: true, // Let MongoDB generate it
+      required: true
+    },
     name: {
       type: String,
       required: [true, "Please add a name"],
@@ -9,17 +14,30 @@ const productSchema = new mongoose.Schema(
     },
     description: {
       type: String,
-      required: [true, "Please add a name"],
+      required: [true, "Please add a description"],
       trim: true
     },
     image: {
       type: String
     },
+
     price: {
-      type: Number,
-      default: 0
+    type: Number,
+    // get: (v) => (v / 100).toFixed(2), // Convert cents to dollars
+    // set: (v) => Math.round(v * 100) ,   // Convert dollars to cents
+    validate: {
+      validator: function(value) {
+        return value > 0;
+      },
+      message: 'Price must be positive'
+      }
     },
-  },
+    currency: {
+      type: String,
+      default: 'USD',
+      enum: ['USD', 'EUR', 'GBP', 'JPY']
+    }
+    },
   { timestamps: true } // adds createdAt, updatedAt
 );
 
